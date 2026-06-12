83f5e38ce5158145ad5eb41c4721857e45de95ce
Murtaza <murtazayshakir@gmail.com>
2026-06-12 14:17:25 +0530

    add sendEmail util

---
+++ utils/sendEmail.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: 'Your OTP Code',
+    html: `<h2>Your OTP is: <b>${otp}</b></h2><p>Valid for 5 minutes.</p>`
+  });
+};
+
+module.exports = sendEmail;
